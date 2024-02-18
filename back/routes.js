44ec0544564986523fb968c943d9d30d11d2f371
bechirzarrouki <bechir.zarrouki00@gmail.com>
2024-02-18 12:25:05 +0100

    test

--- back/routes.js
+++
@@ -1,9 +0,0 @@
-// routes.js
-const express = require('express');
-const router = express.Router();
-const imageController = require('./controllers/imageController'); // Import your controller module
-
-// Route for processing uploaded image and storing data in MongoDB
-router.post('/process-image', imageController.processImage);
-
-module.exports = router;
